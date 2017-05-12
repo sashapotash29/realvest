71b7f03e57ce6c80a694866634c950fb81ff331b
Wes <munsonwa@gmail.com>
2017-05-12 01:05:12 -0400

    more templates and views

--- project/properties/views.py
+++ project/properties/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def show_all(request):
-	return render(request, 'properties.all.html')
+	return render(request, 'properties/properties.html')
 
 def single(request, id):
 	return render(request, 'properties/singleProperty.html')
--- project/user/views.py
+++ project/user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User 
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from .forms import RegistrationForm
 from django.forms.models import model_to_dict
 from properties.models import Properties
@@ -14,6 +14,7 @@ from properties.models import Properties
 def landing_page(request):
 	if request.method == "GET":
 		print(request.body)
+		logout(request)
 		# print(request.GET)
 		args = {'rform':RegistrationForm(), 'lform':AuthenticationForm()}
 		return render(request, 'user/login.html', args)
@@ -41,7 +42,15 @@ def home(request):
 			error = 'The Username and Password you have provided was not correct.'
 			args = {'lform':form,'lError_message':error, 'rform': RegistrationForm()}
 			return render(request, 'user/login.html',args)
-
+	
+	if request.method == "GET":
+		if request.user:
+			args = {'user':request.user}
+			return render(request, 'user/home.html', args)
+		else:
+			error = 'You must first sign in to view that page. If you do not have an account then you must sign up.'
+			args = {'rform':RegistrationForm(),'message':error, 'lform':AuthenticationForm()}
+			return render(request, 'user/login.html', args)
 ######## route /register #####
 
 def register(request):
@@ -52,12 +61,16 @@ def register(request):
 		if form.is_valid():
 			print('valid')
 			form.save()
-			return render(request, 'user/home.html')
+			message = "You have successfully created an account. Login to Get Investing!"
+			args = {'rform':RegistrationForm(),
+					 'lform':AuthenticationForm(),
+					  'message':message
+					  }
+			return render(request, 'user/login.html', args)
 		else:
 			print('not valid')
-			form = RegistrationForm()
 			error = 'The Username you have provided is already taken.'
-			args = {'rform':form,'rError_message':error, 'lform':AuthenticationForm()}
+			args = {'rform':RegistrationForm(),'message':error, 'lform':AuthenticationForm()}
 			return render(request, 'user/login.html', args)
 
 	else:
@@ -68,7 +81,8 @@ def register(request):
 def account_private(request):
 	# print(request.session)
 	# print(dir(request))
-	args = {'user': request.user}
+	form = UserChangeForm
+	args = {'user': request.user, 'form':form}
 	# print(args)
 	return render(request, 'user/personalPage.html', args)
 
