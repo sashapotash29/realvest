4db4c9c55fa8c0b31dd9721ec558ef0deffde17d
Wes <munsonwa@gmail.com>
2017-06-07 16:47:41 -0400

    custom properties admin/property manager

--- project/properties/admin.py
+++ project/properties/admin.py
@@ -2,17 +2,21 @@ from django.contrib import admin
 from properties.models import Properties, Comment, UserProp
 # Register your models here.
 
-class PropertiesAdmin(admin.ModelAdmin):
-	list_display = ('address','city','state','zipcode', 'status')
+#### Customizing Admin Portal
 
+class PropertiesAdmin(admin.ModelAdmin):
+
+	### adding columns
+	list_display = ('address','city','state','zipcode', 'status')
 	def property_info(self, obj):
 		return obj
 
 
-
-
+	### changes order of properties display	
+	def get_queryset(self, request):                                   # method inherited from admin.ModelAdmin class
+		queryset = super(PropertiesAdmin, self).get_queryset(request)  # uses method functionality from inherited class
+		return queryset.order_by('city')                               # admin page currently sorting by city
 
 admin.site.register(Properties, PropertiesAdmin)
 admin.site.register(Comment)
 admin.site.register(UserProp)
-# admin.site.register(PropertiesAdmin)
