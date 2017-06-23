9250735743e36a32a375fa0ea331b20faeabc805
Wes <munsonwa@gmail.com>
2017-06-23 10:44:01 -0400

    message routes

--- project/messenger/views.py
+++ project/messenger/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from django.views import View
+from django.http import JSONResponse
 # Create your views here.
+
+class PostMessageView(View):
+
+
+	def post(eself, request, *args):
+		pass
+
+
+
 def load_conversation(request, id):
  	pass
