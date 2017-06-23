17f4b441f927ec70d1b06e68cc84dbf460fbd2be
Wes <munsonwa@gmail.com>
2017-06-23 10:42:41 -0400

    message routes

--- project/messenger/urls.py
+++ project/messenger/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-	url(r'^load$', views.load_conversation, name="load_message"),
+	url(r'^post$', login_required(PostMessageView.as_view(message)), name="post_message"),
+	url(r'^load$', login_required(LoadMessageView.as_view()), name="load_conversation"),
 ]
