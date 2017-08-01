e6030e3b80a7d22691b22b1e0e67ac5e5bb67d22
sashapotash29 <sashapotash29@gmail.com>
2017-08-01 17:57:45 -0400

    08/01/2017 push, login page tab addition, realtor beginnning endpoints

--- project/realtor/views.py
+++ project/realtor/views.py
@@ -16,6 +16,63 @@ from user.forms import RegistrationForm, EditAccountForm
 # Create your views here.
 
 
-def login(request):
-	params= {}
-	return render(request, 'realtor/login.html', params)
+# def login(request):
+# 	params= {}
+# 	return render(request, 'realtor/login.html', params)
+
+#############################
+######## route /home #####
+
+def home(request):
+	if request.method == "POST":
+		print('==============', request.user)
+		form = AuthenticationForm(request.POST)
+		username =request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request=request, username=username, password=password)
+		print(user)
+		if user is not None:
+			login(request, user)
+			args = {'user':request.user, 'properties': Properties.properties.all()}
+
+			return render(request, 'user/home.html', args)
+		else:
+			error = 'The Username and Password you have provided was not correct.'
+			args = {'lform':form,'lError_message':error, 'rform': RegistrationForm()}
+			return render(request, 'user/login.html',args)
+	
+	if request.method == "GET":
+		if request.user.is_authenticated():
+			args = {'user':request.user, 'properties': Properties.properties.all() }
+			return render(request, 'user/home.html', args)
+		else:
+			error = 'You must first sign in to view that page. If you do not have an account then you must sign up.'
+			args = {'rform':RegistrationForm(),'message':error, 'lform':AuthenticationForm()}
+			return render(request, 'user/login.html', args)
+
+#############################
+######## route /register #####
+
+def register(request):
+	if request.method == "POST":
+		# print(dir(request))
+		form = RegistrationForm(request.POST)
+		print(form)
+		if form.is_valid():
+			print('valid')
+			form.save()
+			message = "You have successfully created an account. Login to Get Investing!"
+			args = {'rform':RegistrationForm(),
+					 'lform':AuthenticationForm(),
+					  'message':message
+					  }
+			return render(request, 'user/login.html', args)
+		else:
+			print('not valid')
+			error = 'The Username you have provided is already taken.'
+			args = {'rform':RegistrationForm(),'message':error, 'lform':AuthenticationForm()}
+			return render(request, 'user/login.html', args)
+
+	else:
+		print('wrong request sent')
+
